4df8b574c3c1eb1035adee3a231ea0d6373a107d
crystalDf <chendong333@gmail.com>
2017-08-20 21:55:57 +0800

    Chapter 04

---
+++ src/ch04/ex01/LabeledPoint.java
@@ -0,0 +1,15 @@
+package ch04.ex01;
+
+public class LabeledPoint extends Point {
+
+    private String label;
+
+    public LabeledPoint(double x, double y, String label) {
+        super(x, y);
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
