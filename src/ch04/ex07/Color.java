04fed348aa1504ce59499409c2bf07a0adf6d705
crystalDf <chendong333@gmail.com>
2017-08-22 21:45:37 +0800

    Chapter 04

---
+++ src/ch04/ex07/Color.java
@@ -0,0 +1,21 @@
+package ch04.ex07;
+
+public enum Color {
+
+    BLACK, RED, BLUE, GREEN, CYAN, MAGENTA, YELLOW, WHITE;
+
+    public Color getRed() {
+
+        return RED;
+    }
+
+    public Color getGreen() {
+
+        return GREEN;
+    }
+
+    public Color getBlue() {
+
+        return BLUE;
+    }
+}
