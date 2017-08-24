13a854f6aa5fe0c57849ebde5cf96ba7b55f89e3
crystalDf <chendong333@gmail.com>
2017-08-24 23:46:52 +0800

    Chapter 04

---
+++ src/ch04/sec05/MethodsTest.java
@@ -0,0 +1,32 @@
+package ch04.sec05;
+
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+public class MethodsTest {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        String className = "java.lang.String";
+
+        Class<?> clazz = Class.forName(className);
+
+        while (clazz != null) {
+
+            for (Method method : clazz.getDeclaredMethods()) {
+
+                System.out.println(
+
+                        Modifier.toString(method.getModifiers()) + " " +
+                                method.getReturnType().getCanonicalName() + " " +
+                                method.getName() +
+                                Arrays.toString(method.getParameters())
+                );
+            }
+
+            clazz = clazz.getSuperclass();
+        }
+    }
+}
