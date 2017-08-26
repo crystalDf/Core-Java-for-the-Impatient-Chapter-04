50bef99ecbbb3320bb5fab9976bd2fce49e594fa
crystalDf <chendong333@gmail.com>
2017-08-26 19:31:42 +0800

    Chapter 04

--- src/ch04/ex13/Ex13.java
+++ src/ch04/ex13/Ex13.java
@@ -2,6 +2,7 @@ package ch04.ex13;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.function.DoubleFunction;
 
 public class Ex13 {
 
@@ -18,13 +19,31 @@ public class Ex13 {
         printMethodValue(method, 1, 100, 10);
 
         System.out.println();
+
+        printDoubleFunctionValue(1, 100, 10, Math::sqrt);
+
+        System.out.println();
+
+        printDoubleFunctionValue(1, 100, 10, Double::toHexString);
+
+        System.out.println();
+
     }
 
     public static void printMethodValue(Method method,
-                                        double low, double high, double step) throws InvocationTargetException, IllegalAccessException {
+                                        double low, double high, double step)
+            throws InvocationTargetException, IllegalAccessException {
 
         for (double value = low; value < high; value += step) {
             System.out.println(method.invoke(null, value));
+        }
+    }
+
+    public static void printDoubleFunctionValue(double low, double high, double step,
+                                                DoubleFunction doubleFunction) {
+
+        for (double value = low; value < high; value += step) {
+            System.out.println(doubleFunction.apply(value));
         }
     }
 }
